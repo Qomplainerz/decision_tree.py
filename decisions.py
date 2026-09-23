6c046109024c00811f8feb4dbcd41967a59083bd
Qomplainerz <messanyrecordings@yahoo.de>
2026-09-24 00:28:12 +0200

    Update decisions.py

--- decisions.py
+++ decisions.py
@@ -5,3 +5,10 @@ if name == "Adeptus" and mood == "Happy":
     print(f"{name} is happy.")
 else:
     print(f"{name} is not happy.")
+
+adeptus_has_pets = False
+
+if adeptus_has_pets:
+    print(f"{name} has pets.")
+else:
+    print(f"{name} does not have pets.")
