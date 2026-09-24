f44264b4c07c7ecdfdb0c80376da64ed2c25dc4b
Qomplainerz <messanyrecordings@yahoo.de>
2026-09-24 10:29:03 +0200

    Add age check to decisions.py

--- decisions.py
+++ decisions.py
@@ -12,3 +12,10 @@ if adeptus_has_pets:
     print(f"{name} has pets.")
 else:
     print(f"{name} does not have pets.")
+
+age = 25
+
+if age < 18:
+    print(f"{name} is a minor.")
+else:
+    print(f"{name} is an adult.")
